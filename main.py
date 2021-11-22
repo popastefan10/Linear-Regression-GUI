4c455a3715eea26e9bf686d0dd8d8eb84756a3d0
Stefan Popa <popashtefan10@gmail.com>
2021-11-22 17:49:10 +0200

    Modified main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from MainApp import MainApplication
+from tkinter import Tk
+
+def main():
+    root = Tk()
+    root.title("Linear regression in python")
+    min_root_width = 400
+    min_root_height = 500
+    root.minsize(min_root_width, min_root_height)
+
+    root.update()
+    MainApp = MainApplication(root, width=root.winfo_width(), height=root.winfo_height(), bg="white")
+    MainApp.grid_propagate(0)
+    MainApp.grid(row=0, column=0)
+
+
+    def on_resize(event):
+        """
+        Callback used to resize MainApp when the master window is resized.
+        """
+
+        new_width = max(event.width, min_root_width)
+        new_height = max(event.height, min_root_height)
+        MainApp["width"] = new_width
+        MainApp["height"] = new_height
+
+    root.bind("<Configure>", on_resize)
+
+
+    def refresh_plot_data():
+        """
+        Callback used to get the plot data and display it on the main app.
+        """
+
+        if MainApp.plot is not None:
+            plot_points = MainApp.plot.points
+            plot_lin_reg = MainApp.plot.lin_reg
+            already_added_points = MainApp.plot_data.points
+
+            for i in range(len(already_added_points), len(plot_points)):
+                # Add only new points
+                new_point = plot_points[i]
+                MainApp.plot_data.update_data(new_point.x, new_point.y, 
+                                            plot_lin_reg.get_alpha(), plot_lin_reg.get_beta())
+
+        root.after(500, refresh_plot_data)
+
+
+    root.after(100, refresh_plot_data)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
